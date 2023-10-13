e2a9ab8ceef2e58bf5d8f69ec1630738cb1b596e
putubagus13 <putu031216@gmail.com>
2023-10-13 09:00:37 +0700

    fix: layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import PropTypes from 'prop-types';
 
 function Layout({children}) {
   return (
-    <React.Fragment>
-      <div className='h-full w-full bg-white text-black'>
-        <Header />
-        {children}
-        <Footer />
-      </div>
-    </React.Fragment>
+    <div className='h-full w-full bg-white text-black'>
+      <Header />
+      {children}
+      <Footer />
+    </div>
   );
 }
 Layout.propTypes = {
